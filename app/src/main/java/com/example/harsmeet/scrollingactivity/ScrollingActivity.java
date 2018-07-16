76be8d723152601f83f14502b8a01c953dff8ed4
harsmeet <er.harsmeet@gmail.com>
2018-07-16 11:15:40 +0530

    OnClick TextView Calling Option in Scrolling Activity

--- app/src/main/java/com/example/harsmeet/scrollingactivity/ScrollingActivity.java
+++ app/src/main/java/com/example/harsmeet/scrollingactivity/ScrollingActivity.java
@@ -1,37 +1,39 @@
 package com.example.harsmeet.scrollingactivity;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_PHONE_CALL = 10000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Scrolling Activity");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setImageResource(R.drawable.ic_interaction2);
-=======
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setImageResource(R.drawable.ic_interaction2);
->>>>>>> 46b91ad3db27dba96f7ed319c6559ffc3a30c454
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +66,83 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
 
+//        Edit Text Phone number Email
+
+        TextView phoneNumber = (TextView) findViewById(R.id.et_number);
+        TextView email = (TextView) findViewById(R.id.et_email);
+
+        phoneNumber.setText("Phone Number: " + "9999999999");
+
+        email.setText("Email Id: " + "testing@rediff.com");
+
+
+        phoneNumber.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(ScrollingActivity.this," Phone Number Clicked !",Toast.LENGTH_SHORT).show();
+
+                call();
+
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+
+                    Toast.makeText(ScrollingActivity.this," Email Id Clicked !",Toast.LENGTH_SHORT).show();
+
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + "testing@rediff.com"));
+                    startActivity(intent);
+
+                } catch (ActivityNotFoundException ignored) {
+
+                }
+            }
+        });
+
     }
+
+
+//    For Calling PRocess
+
+    private void call() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(ScrollingActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST_PHONE_CALL);
+        } else {
+            //Open call function
+//            String number = myClientsDataSets.getContact_no();
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + "9999999999"));
+            startActivity(intent);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_PHONE_CALL) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+
+//                call(myClientsDataSets);
+
+                call();
+
+            } else {
+                Toast.makeText(ScrollingActivity.this, "Sorry!!! Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 
 
     @Override
